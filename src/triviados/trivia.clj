2d378ae3bfc1381938f57d3ad6470986818ef2ca
Oskar Zabik <oskar.zabik@gmail.com>
2022-06-13 14:46:49 +0100

    fix entities

--- src/triviados/trivia.clj
+++ src/triviados/trivia.clj
@@ -1,7 +1,29 @@
 (ns triviados.trivia
-  (:require [triviados.http :as http]))
+  (:require [triviados.http :as http]
+            [clojure.string :as str]))
 
 (def api-url "https://opentdb.com/api.php")
+
+(def entities->char
+  {"&quot;" "\""
+   "&#039;" "'"
+   "&amp;" "&"})
+
+(defn map->kws-as-regex [m]
+  (re-pattern
+   (reduce-kv (fn [s k _]
+                (if s
+                  (str s "|" k)
+                  k))
+              nil
+              m)))
+
+(defn replace-several [s replacements-map]
+  (str/replace s (map->kws-as-regex replacements-map) replacements-map))
+
+(defn fix-entities [s]
+  (replace-several s entities->char)
+  )
 
 (defn get-api-question-url [amount]
   (str api-url "?amount=" amount))
@@ -9,7 +31,14 @@
 (defn get-questions [amount]
   (let [url (get-api-question-url amount)
         {:keys [results]} (http/http-get url)]
-    results))
+    (map (fn [question]
+           (-> question
+               (update :question fix-entities)
+               (update :correct-answer fix-entities)
+               (update :incorrect-answers (fn [incorrect-answers]
+                                            (map fix-entities incorrect-answers)
+                                            ))))
+         results)))
 
 (comment
   (get-questions 1)
