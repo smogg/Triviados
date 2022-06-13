91038d36e30852619274579e2f51f39b33476788
Oskar Zabik <oskar.zabik@gmail.com>
2022-06-13 15:25:50 +0100

    refactoring

--- src/triviados/state.clj
+++ src/triviados/state.clj
@@ -12,16 +12,13 @@
     (swap! !state update :questions into questions)))
 
 (defn get-last-question! []
-  (if (-> @!state :questions count pos?)
-    ;; if there are questions, get last
-    (let [last-question (-> @!state :questions last)]
-      (swap! !state (fn [state]
-                      (-> state
-                          (update :questions pop)
-                          (update :question-counter inc)
-                          (assoc :current-question last-question))))
-      last-question)
-    ;; otherwise, fetch more questions first
-    (do
-      (fetch-more-questions!)
-      (get-last-question!))))
+  ;; fetch more questions if required
+  (when-not (-> @!state :questions count pos?)
+    (fetch-more-questions!))
+  (let [last-question (-> @!state :questions last)]
+    (swap! !state (fn [state]
+                    (-> state
+                        (update :questions pop)
+                        (update :question-counter inc)
+                        (assoc :current-question last-question))))
+    last-question))
